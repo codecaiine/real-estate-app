042256e8f45c66df8c6642c857030135aa974c63
codecaiine <akabrouyannickn@gmail.com>
2021-12-04 23:50:55 +0000

    Create Banner component

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,18 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
+
+const Banner =()=> (
+  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+    <Image src="{imageUrl}" width={500} height={300} alt="banner" />
+
+  </Flex>
+)
+
 export default function Home() {
   return (
-    <div className={styles.container}>
-
+    <div>
+        <h1>Hello world</h1>
     </div>
   )
 }
