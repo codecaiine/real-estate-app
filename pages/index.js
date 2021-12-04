6c3b5e6e3c3dbde452ca1e7bb4bd9f0ed8344103
codecaiine <akabrouyannickn@gmail.com>
2021-12-04 23:57:54 +0000

    Add box text button components

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,12 @@ import { Flex, Box, Text, Button } from '@chakra-ui/react';
 const Banner =()=> (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
     <Image src="{imageUrl}" width={500} height={300} alt="banner" />
-
+    <Box p="5">
+      <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
+      <Text fontSize="3xl" fontWeight="medium">{title1}<br />{title2}</Text>
+      <Text fontSize="lg" paddingTop="3" paddingBottom="3" color="gray.700">{descr1}<br />{descr2}</Text>
+      <Button></Button>
+    </Box>
   </Flex>
 )
 
