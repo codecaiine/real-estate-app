ebdb0cd27bf327ff205e591e6ea650eab8be6518
codecaiine <akabrouyannickn@gmail.com>
2021-12-05 23:58:34 +0000

    Add fetchApi function

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
-cont baseUrl = 'bayut.p.rapidapi.com';
+const baseUrl = 'bayut.p.rapidapi.com';
 
-// headers: {
-//    'x-rapidapi-host': 'bayut.p.rapidapi.com',
-//    'x-rapidapi-key': 'bfada36e05msh8af417e0b315d02p1d6b82jsn4981e0bf00d1'
-// }
+export const fetchApi = async (url) => {
+    const response = await axios.get((url), {
+        headers: {
+            'x-rapidapi-host': 'bayut.p.rapidapi.com',
+            'x-rapidapi-key': 'bfada36e05msh8af417e0b315d02p1d6b82jsn4981e0bf00d1'
+         }
+    }) 
+}
