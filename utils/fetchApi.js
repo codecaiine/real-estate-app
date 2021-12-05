c6cde007bf6413724da87bbb2dbf0d2e15f3e55d
codecaiine <akabrouyannickn@gmail.com>
2021-12-05 23:52:23 +0000

    Add baseUrl constante

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+cont baseUrl = 'bayut.p.rapidapi.com';
+
 // headers: {
 //    'x-rapidapi-host': 'bayut.p.rapidapi.com',
 //    'x-rapidapi-key': 'bfada36e05msh8af417e0b315d02p1d6b82jsn4981e0bf00d1'
