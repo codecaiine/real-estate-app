4b95849d70203a586ddfb5aa79c6834322715017
codecaiine <akabrouyannickn@gmail.com>
2021-12-06 23:34:08 +0000

    Import necessary dependencies

---
+++ components/Property.jsx
@@ -0,0 +1,7 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import {Box, Flex, Text, Avatar} from '@chakra-ui/react';
+import {FaBed, FaBath} from 'react-icons/fa';
+import {BsGridFill} from 'react-icons/bs';
+import {GoVerified} from 'react-icons/goverified';
+import millify from 'millify';
