563749df3d2354496d0199a00b85198562561ce7
codecaiine <akabrouyannickn@gmail.com>
2021-12-07 21:51:31 +0000

    Add image to property

--- components/Property.jsx
+++ components/Property.jsx
@@ -5,6 +5,7 @@ import {FaBed, FaBath} from 'react-icons/fa';
 import {BsGridFill} from 'react-icons/bs';
 import {GoVerified} from 'react-icons/goverified';
 import millify from 'millify';
+
 import DefaultImage from '../assets/images/house.jpg';
 
 const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID}}) => (
